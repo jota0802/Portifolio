179930403a6d87730e3823f4c86a815a15a7c488
Jota <jvfranco08@gmail.com>
2024-12-12 16:17:48 -0300

    estrutura de rotas criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
+import Nav from "./components/Nav/Nav"
+import Footer from "./components/Footer/Footer"
+import { Outlet } from "react-router-dom"
 function App() {
   return (
     <>
-    
+    <Nav />
+    <Outlet />  
+    <Footer />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Error from './routes/Error/Error.jsx'
+import Home from './routes/Home/Home.jsx'
+import About from './routes/About/About.jsx'
+import Projects from './routes/Projects/Projects.jsx'
 import App from './App.jsx'
 
+
+const router = createBrowserRouter([{
+
+  path: '/', element: <App />,
+  errorElement: <Error />, 
+
+  children:[
+    {path: '/', element: <Home />},
+    {path: '/About', element: <About />},
+    {path: '/Projects', element: <Projects />},
+  ]
+
+}])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
